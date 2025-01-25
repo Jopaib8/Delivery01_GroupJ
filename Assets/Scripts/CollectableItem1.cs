f73cee72cdc66c330ba49b39844a21ffa58814b5
Jopaib8 <jopaib8@gmail.com>
2025-01-25 17:22:05 +0100

    Trigger and Collectable Items
    Collectable Items started

---
+++ Assets/Scripts/CollectableItem1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using UnityEngine;
+
+public class ColletableItem : MonoBehaviour
+{
+
+
+    [SerializeField] private AudioClip colect1;
+
+
+    // Inicializa el objeto
+    private void Awake()
+    {
+        //thisobject = GetComponent<GM_Item>();
+    }
+    // Si el jugador colisiona con el objeto, se destruye y se agrega al inventario
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player collided with collectible");
+
+            Destroy(gameObject);
+            
+            /*if (SoundController.Instance != null)
+            {
+                SoundController.Instance.EjecutarSonido(colect1);
+            }
+            else
+            {
+                Debug.LogError("SoundController instance is null");
+            }*/
+        }
+    }
+}
